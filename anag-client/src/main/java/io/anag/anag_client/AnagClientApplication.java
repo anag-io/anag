a47f032f56f75eb7b434ca44261879e191b95854
Vibhor Agarwal <vibhoragarwal@Vibhors-MBP.home>
2024-10-18 16:45:48 +0200

    crated strrcture

---
+++ anag-client/src/main/java/io/anag/anag_client/AnagClientApplication.java
@@ -0,0 +1,13 @@
+package io.anag.anag_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnagClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnagClientApplication.class, args);
+	}
+
+}
---
+++ anag-client/src/test/java/io/anag/anag_client/AnagClientApplicationTests.java
@@ -0,0 +1,13 @@
+package io.anag.anag_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnagClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
